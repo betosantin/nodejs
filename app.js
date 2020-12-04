456e6ca51631fa3600df5da0613f89d2e19bb562
Roberto Santin <betosantin@gmail.com>
2020-12-04 09:10:28 -0300

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var calculadora = require("./calc");
+
+console.log(calculadora.mult(2,4));
+console.log(calculadora.sub(5,3));
+console.log(calculadora.div(5,0));
+console.log(calculadora.soma(4,10));
